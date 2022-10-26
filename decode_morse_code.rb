84490d3c1acfbea4c597ea4c2d44b021eb75ffd6
David-Kasilia <davidkasilia001@gmail.com>
2022-10-26 16:08:43 +0300

    Added Function to decode morse code

--- decode_morse_code.rb
+++ decode_morse_code.rb
@@ -1,43 +1,53 @@
 def decode_char(str)
   # Method to decode single Morse code character
   morse_code_dict = {
-  '.-' => 'A',
-  '-...' => 'B',
-  '-.-.' => 'C',
-  '-..' => 'D',
-  '.' => 'E',
-  '..-.' => 'F',
-  '--.' => 'G',
-  '....' => 'H',
-  '..' => 'I',
-  '.---' => 'J',
-  '-.-' => 'K',
-  '.-..' => 'L',
-  '--' => 'M',
-  '-.' => 'N',
-  '---' => 'O',
-  '.--.' => 'P',
-  '--.-' => 'Q',
-  '.-.' => 'R',
-  '...' => 'S',
-  '-' => 'T',
-  '..-' => 'U',
-  '...-' => 'V',
-  '.--' => 'W',
-  '-..-' => 'X',
-  '-.--' => 'Y',
-  '--..' => 'Z'
-}
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
+  }
   morse_code_dict[str]
 end
 
 def decode_word(str)
-  strArr = str.split
+  str_arr = str.split
   output = ''
-  strArr.each {
-    |char| output += decode_char(char)
-  }
-  return output + " "
+  str_arr.each do |char|
+    output += decode_char(char)
+  end
+  "#{output} "
 end
 
-print decode_word("-- -.--")
+def decode(str)
+  words_arr = str.split(/   /)
+  final_output = ''
+  words_arr.each do |word|
+    final_output += decode_word(word)
+  end
+  final_output
+end
+
+# print decode("-- -.--   -. .- -- .")
+print decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
